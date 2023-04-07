d4f1ff63395d87d47bed3fc85beece61897512e4
zooyeong <zooyeong@icloud.com>
2023-04-07 17:19:17 +0900

    1차 프로젝트 최종 완료

--- mainscript.js
+++ mainscript.js
@@ -1,30 +1,19 @@
-//current position
 let pos = 0;
-//number of slides
 let totalSlides = $('#slider-wrap ul li').length;
-//get the slide width
 let sliderWidth = $('#slider-wrap').width();
 
 $(document).ready(function(){
 	
-	
-	/*****************
-	BUILD THE SLIDER
-	*****************/
-	//set width to be 'x' times the number of slides
 	$('#slider-wrap ul#slider').width(sliderWidth*totalSlides);
 	
-    //next slide 	
 	$('#next').click(function(){
 		slideRight();
 	});
 	
-	//previous slide
 	$('#previous').click(function(){
 		slideLeft();
 	});
 	
-	//automatic slider
 	let autoSlider = setInterval(slideRight, 3000);
 	
 	$.each($('#slider-wrap ul li'), function() { 
@@ -32,10 +21,6 @@ $(document).ready(function(){
 		$('#pagination-wrap ul').append(li);	   
 	});
 	
-	//counter
-	// countSlides();
-	
-	//pagination
 	pagination();
 	
 	$('#slider-wrap').hover(
@@ -43,49 +28,23 @@ $(document).ready(function(){
 		function(){ $(this).removeClass('active'); autoSlider = setInterval(slideRight, 3000); }
 	);
 
-});//DOCUMENT READY
+});
 	
-
-
-/***********
-	SLIDE LEFT
-************/
 function slideLeft(){
 	pos--;
 	if(pos==-1){ pos = totalSlides-1; }
 	$('#slider-wrap ul#slider').css('left', -(sliderWidth*pos)); 	
-	
-	//*> optional
-	// countSlides();
 	pagination();
 }
 
-
-/************
-	SLIDE RIGHT
-*************/
 function slideRight(){
 	pos++;
 	if(pos==totalSlides){ pos = 0; }
 	$('#slider-wrap ul#slider').css('left', -(sliderWidth*pos)); 
-	
-	//*> optional 
-	// countSlides();
 	pagination();
 }
-
-
-
-	
-/************************
- //*> OPTIONAL SETTINGS
-************************/
-// function countSlides(){
-	// $('#counter').html(pos+1 + ' / ' + totalSlides);
-// }
 
 function pagination(){
 	$('#pagination-wrap ul li').removeClass('active');
 	$('#pagination-wrap ul li:eq('+pos+')').addClass('active');
 }
-
